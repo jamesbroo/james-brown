6d236900d4000ec11f2a135919429a970453580b
raxioma <raxioma@outlook.com>
2022-07-09 15:30:32 +1000

    about update

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "I'm an aspiring blockchain developer who has 4+ years experience in smart contract, full stack engineering, frontend and back-end application development.",
+    description1: "I'm an aspiring blockchain developer who has 5+ years experience in smart contract, full stack engineering, frontend and back-end application development. I have about 8+ years of experience in software development.",
     description2: "I have an abundance of experience in web development with a focus on Solidity, Web3, JavaScript, Typescript and React. Moreover, I have in-depth knowledge and relevant experience in distributed ledgers, blockchain security, architectures, standards and ecosystems.",
     description3: "For recent years, I've collaborated with a variety of companies including startups and medium communities making me intimately familiar with all the aspects of the web life cycle from the initial architecture to final deployment.",
     image: 2
